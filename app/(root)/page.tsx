e26f1269a2025cce4506a61670652af05df49e84
John Aricsson Majumder <johnaricsson1@gmail.com>
2025-10-30 14:45:19 +0600

    built header and search form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,22 @@
 import Image from "next/image";
+import SearchForm from "../../components/SearchForm";
 
-export default function Home() {
+export default async function Home({searchParams}:{
+  searchParams: Promise <{query?: string}>}
+)
+{
+  const query = (await searchParams).query;
+ 
   return (
     <>
-    <h1 className="text-3xl">Home</h1>
+    <section className="pink_container pattern">
+      <h1 className="heading">Pitch Your Startup, <br />Connect With Founders and Entrepreneurs</h1>
+      <p className="sub-heading !max-w-3xl">
+        Where founders connect with peers to rate, refine, and launch ideas.
+      </p>
+      <SearchForm query={query}/>
+    </section>
+
     </>
   );
 }
