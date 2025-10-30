3b5603070aeeeacf099f3050f97f62eb2648e6c6
John Aricsson Majumder <johnaricsson1@gmail.com>
2025-10-30 23:51:43 +0600

    Buiild start up cards

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,22 +1,52 @@
 import Image from "next/image";
 import SearchForm from "../../components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
-export default async function Home({searchParams}:{
-  searchParams: Promise <{query?: string}>}
-)
-{
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
   const query = (await searchParams).query;
- 
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "John Doe" },
+      _id: 1,
+      description: "This is a description",
+      image:
+        "https://tds-images.thedailystar.net/sites/default/files/styles/big_202/public/images/2025/03/23/blue_droid.png",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
   return (
     <>
-    <section className="pink_container pattern">
-      <h1 className="heading">Pitch Your Startup, <br />Connect With Founders and Entrepreneurs</h1>
-      <p className="sub-heading !max-w-3xl">
-        Where founders connect with peers to rate, refine, and launch ideas.
-      </p>
-      <SearchForm query={query}/>
-    </section>
-
+      <section className="pink_container pattern">
+        <h1 className="heading">
+          Pitch Your Startup, <br />
+          Connect With Founders and Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Where founders connect with peers to rate, refine, and launch ideas.
+        </p>
+        <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType) => (
+              <StartupCard key={post._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found </p>
+          )}
+        </ul>
+      </section>
     </>
   );
 }
